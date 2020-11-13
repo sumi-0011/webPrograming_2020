e9a1c2b0fbd2269c3a5373589edeb163b0dc7721
sumi <selina2000@naver.com>
2020-11-13 18:03:48 +0900

    temP menu

---
+++ TermProject/menu/menu_load.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("header").load("./menu/header.html");
+    $("nav").load("./menu/nav.html");
+    $("aside").load("./menu/aside.html");
+    $("footer").load("./menu/footer.html");
+})
